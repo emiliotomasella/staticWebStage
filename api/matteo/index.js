013cbf77af7fc7cdebe13194e18f75f688d786c8
emiliotomasella <98945031+emiliotomasella@users.noreply.github.com>
2022-02-04 10:52:08 +0100

    Add files via upload

--- api/matteo/index.js
+++ api/matteo/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
     context.res.json({
-        "Nome" : "Matteo" , "Cognome" : "Saitta" 
+        Nome : "Matteo" , Cognome : "Saitta" 
     });
 };  
