201236592322c8539be3502c9ad5a13bb9667ad4
emiliotomasella <98945031+emiliotomasella@users.noreply.github.com>
2022-02-04 11:39:59 +0100

    Add files via upload
    update delle api

--- api/emilio/index.js
+++ api/emilio/index.js
@@ -1,5 +1,6 @@
 module.exports = async function (context, req) {
     context.res.json({
-        text: "Nome: Emilio\nCognome: Tomasella"
+        nometec: "Emilio",
+        cognometec: "Tomasella" 
     });
 };  
--- api/guido/index.js
+++ api/guido/index.js
@@ -1,5 +1,6 @@
 module.exports = async function (context, req) {
     context.res.json({
-        text: "Nome: Guido\nCognome: Cosulich"
+        nometec: "Guido",
+        cognometec: "Cosulich" 
     });
 };  
