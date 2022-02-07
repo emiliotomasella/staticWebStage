eb7437e5ff8c9f745b0267ee9f09bd329eec4ab6
emiliotomasella <98945031+emiliotomasella@users.noreply.github.com>
2022-02-07 10:40:29 +0100

    Add files via upload

--- api/emilio/index.js
+++ api/emilio/index.js
@@ -3,6 +3,6 @@ module.exports = async function (context, req) {
         nometec: "Emilio",
         cognometec: "Tomasella",
         ruolo: "stagista",
-        percorsoimmagine: "https://th.bing.com/th/id/OIP.ZT-Tw8tYy38htqch69vsGQAAAA?pid=ImgDet&rs=1"
+        immagine: "https://th.bing.com/th/id/OIP.ZT-Tw8tYy38htqch69vsGQAAAA?pid=ImgDet&rs=1"
     });
 };  
